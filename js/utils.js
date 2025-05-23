8f4e66eda79fbe1f3824f497d4d16dce71b687fc
btwmarketing79 <148632717+btwmarketing79@users.noreply.github.com>
2025-05-23 18:07:11 +0200

    Fix: remove illegal top-level return and clean up getMarketPrice

--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,5 @@
-return (
-    prices.normal?.market ||
-    prices.holofoil?.market ||
-    prices.reverseHolofoil?.market ||
-    prices['1stEditionHolofoil']?.market ||
-    null
-  );
-};
-
 export const getMarketPrice = (card) => {
-  const prices = card.tcgplayer?.prices || {};
+  const prices = card?.tcgplayer?.prices || {};
   return (
     prices.normal?.market ??
     prices.holofoil?.market ??
